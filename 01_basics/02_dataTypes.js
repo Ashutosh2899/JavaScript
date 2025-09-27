8e5d5f6d6d111e86402b823d555774863714f5bb
Ashutosh2899 <bhardwaj9619@gmail.com>
2025-09-27 19:52:05 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer version
+
+// alert("hello") we are using node js, not browser
+
+let name = "Ashutosh"; //string
+let age = 18; // number
+let islogin = false // boolean
+let state; // undefined
+let tem = null; // null
+
+//number => 2 to power 53
+//bigint
+//string => "" or ''
+//boolean => true or false
+//null => standalone value bhi hai 
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
