8716f77341a9a366589c4c08415a2e1c116e6fcf
Ashutosh2899 <bhardwaj9619@gmail.com>
2025-09-27 19:30:18 +0530

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345;
+let accountEmail = "account@gmail.com";
+var accountPassword = 45210;
+accountCity = "mumbai";
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
