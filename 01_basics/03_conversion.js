d3e234ac085e10add3d6eab3fd50387a6a6e8f88
Ashutosh2899 <bhardwaj9619@gmail.com>
2025-09-27 20:30:11 +0530

    Conversion & Operations

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,44 @@
+let score = 33;
+let score2 = "33";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score2) // convert string into number
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+// "" => false
+// "Ashu" => true
+
+let islogin = 1;
+// console.log(typeof islogin);
+
+let booleanIsLogin = Boolean(islogin);
+// console.log(typeof booleanIsLogin);
+
+
+
+let someNumber = 33;
+// console.log(typeof someNumber);
+
+let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************** Operations **************************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);                
+// console.log(2**2); 
+
+// prefix or postfix
+
+
+
+
+ 
+
