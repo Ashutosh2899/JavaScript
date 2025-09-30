8920f706ff8cee814e5f2cf80b988caccaa61eb2
Ashutosh2899 <bhardwaj9619@gmail.com>
2025-09-30 11:59:43 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,15 @@
+const name = "ashu";
+const repoCount = 50;
+
+// console.log(name + repoCount + "value" ); // outdated
+
+// console.log(`my name is ${name} and my repo count is ${repoCount}`); // string interpolation
+
+const gameName = new String("Ashutosh");
+
+// string method 
+
+console.log(gameName.length);
+
+
+//  learn all method on console 
---
+++ Interview/dataType-Summary.js
@@ -0,0 +1,14 @@
+// JavaScript is generally considered a weakly and dynamically typed language, not a strongly typed one
+
+// how many type of datatype 
+// there are two type to datatype 
+// 1) primitive 
+// 7 types: string, number, boolean, null, undefined, bigint, symbol
+
+// 2) nonprimitive (reference)
+// object, array, functions
+
+ 
+// memory 
+
+// stack (primitive), heap (non-primitive)  
