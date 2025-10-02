9e08697c6cb07efa933dbe6821d0c5379e92c8ed
Ashutosh2899 <bhardwaj9619@gmail.com>
2025-10-02 12:47:37 +0530

    date & time

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+
+let myCreatedDate = new Date(2025, 0, 2);
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+
